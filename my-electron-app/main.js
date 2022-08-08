349dc0649f372742d5493d215891e2ab0ad6a815
Andy Chen <chenyangpoppin@hotmail.com>
2022-08-08 14:58:06 +0800

    feat: location an index.html page locally

--- my-electron-app/main.js
+++ my-electron-app/main.js
@@ -1,1 +1,17 @@
+const { app, BrowserWindow } = require('electron');
+
+const createWindow = () => {
+  // Create and control browser windows with BrowserWindow
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600
+  });
+
+  win.loadFile('index.html');
+};
+
+app.whenReady().then(() => {
+  createWindow();
+});
+
 console.log(`Hello from Electron 👋`);
