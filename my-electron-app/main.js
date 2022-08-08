ee71c5347000960598bcd16623dceb660933554e
Andy Chen <chenyangpoppin@hotmail.com>
2022-08-08 15:16:37 +0800

    feat: fill the gap among different platforms

--- my-electron-app/main.js
+++ my-electron-app/main.js
@@ -1,3 +1,9 @@
+/**
+ * An electron app covers MacOS, Window, and Linux.
+ * 
+ * There are slightly difference between them
+ * 
+ */
 const { app, BrowserWindow } = require('electron');
 
 const createWindow = () => {
@@ -12,6 +18,23 @@ const createWindow = () => {
 
 app.whenReady().then(() => {
   createWindow();
+
+  // MacOS apps generally continue running even without any windows open.
+  // Activating the app when no windows are available should open a new one.
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+// On Windows and Linux, closing all windows will generally quit an application entirely.
+// MacOS closes all windows does not necessarily mean close the app
+// But on Window and Linux, closing all the windows means close the app
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
 });
 
 console.log(`Hello from Electron 👋`);
